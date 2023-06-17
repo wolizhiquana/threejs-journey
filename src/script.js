fc0a95fd4d8fc8bba1c6dc6ad7922eaf041e646d
wolizhiquana <wolizhiquana@gmail.com>
2023-06-18 01:30:12 +0800

    raycaster and mouse event

--- src/script.js
+++ src/script.js
@@ -1,8 +1,7 @@
-import * as dat from 'lil-gui'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as dat from 'lil-gui'
 
 /**
  * Base
@@ -19,54 +18,52 @@ const scene = new THREE.Scene()
 /**
  * Models
  */
-const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('/draco/')
+let model = null
 
 const gltfLoader = new GLTFLoader()
-gltfLoader.setDRACOLoader(dracoLoader)
-
-let mixer = null
-
-gltfLoader.load('/models/Fox/glTF/Fox.gltf', (gltf) => {
-  mixer = new THREE.AnimationMixer(gltf.scene)
-  const action = mixer.clipAction(gltf.animations[2])
-
-  action.play()
-
-  gltf.scene.scale.set(0.025, 0.025, 0.025)
+gltfLoader.load('models/Duck/glTF/Duck.gltf', (gltf) => {
+  model = gltf.scene
+  gltf.scene.position.y = -1.2
   scene.add(gltf.scene)
 })
+
 /**
- * Floor
+ * Objects
  */
-const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshStandardMaterial({
-    color: '#444444',
-    metalness: 0,
-    roughness: 0.5
-  })
+const object1 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
-floor.receiveShadow = true
-floor.rotation.x = -Math.PI * 0.5
-scene.add(floor)
+object1.position.x = -2
+
+const object2 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+
+const object3 = new THREE.Mesh(
+  new THREE.SphereGeometry(0.5, 16, 16),
+  new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object3.position.x = 2
+
+scene.add(object1, object2, object3)
 
 /**
- * Lights
+ * Ray caster
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
-scene.add(ambientLight)
+const raycaster = new THREE.Raycaster()
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = -7
-directionalLight.shadow.camera.top = 7
-directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = -7
-directionalLight.position.set(5, 5, 5)
-scene.add(directionalLight)
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0)
+// rayDirection.normalize()
+
+// raycaster.set(rayOrigin, rayDirection)
+
+// const intersect = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersect)
+
+// scene.add(raycaster)
 
 /**
  * Sizes
@@ -91,6 +88,32 @@ window.addEventListener('resize', () => {
 })
 
 /**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.onmousemove = (e) => {
+  mouse.x = (e.clientX / sizes.width) * 2 - 1
+  mouse.y = -((e.clientY / sizes.height) * 2) + 1
+}
+
+window.onclick = (e) => {
+  if (currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1:
+        console.log('click on object1')
+        break
+      case object2:
+        console.log('click on object2')
+        break
+      case object3:
+        console.log('click on object3')
+        break
+    }
+  }
+}
+
+/**
  * Camera
  */
 // Base camera
@@ -100,12 +123,21 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 )
-camera.position.set(2, 2, 2)
+camera.position.z = 3
 scene.add(camera)
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.3)
+scene.add(ambientLight)
+
+const directionLight = new THREE.DirectionalLight('#ffffff', 0.7)
+directionLight.position.set(1, 2, 3)
+scene.add(directionLight)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0.75, 0)
 controls.enableDamping = true
 
 /**
@@ -114,8 +146,6 @@ controls.enableDamping = true
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 })
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -123,15 +153,46 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
-let previousTime = 0
+
+let currentIntersect = null
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime()
-  const deltaTime = elapsedTime - previousTime
-  previousTime = elapsedTime
 
-  // Update mixer
-  mixer?.update(deltaTime)
+  // Animate objects
+  object1.position.y = Math.sin(elapsedTime * 0.2) * 1.5
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+
+  // // Cast a ray
+  raycaster.setFromCamera(mouse, camera)
+
+  const objectsToTest = [object1, object2, object3]
+  const intersects = raycaster.intersectObjects(objectsToTest)
+
+  objectsToTest.forEach((obj) => {
+    obj.material.color.set('#ff0000')
+  })
+
+  intersects.forEach((intersect) => {
+    intersect.object.material.color.set('#0000ff')
+  })
+
+  if (intersects.length) {
+    if (!currentIntersect) console.log('mouse enter')
+
+    currentIntersect = intersects[0]
+  } else {
+    if (currentIntersect) console.log('mouse leave')
+    currentIntersect = null
+  }
+
+  // Test Intersect with model
+  if (model) {
+    const modelIntersects = raycaster.intersectObject(model)
+    if (modelIntersects.length) model.scale.set(1.2, 1.2, 1.2)
+    else model.scale.set(1, 1, 1)
+  }
 
   // Update controls
   controls.update()
