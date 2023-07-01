d4fe07c42ff39b6c2eb6040c9154cd9b05689630
wolizhiquana <wolizhiquana@gmail.com>
2023-07-01 11:05:29 +0800

    r3f portal sence - repaire build bug

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,12 +1,8 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useRef } from 'react'
-import * as THREE from 'three'
 import Model from './Model'
 
 function Experience() {
-  const icosahedron = useRef<THREE.Mesh>(null!)
-
   return (
     <>
       <Perf position='top-left' />
