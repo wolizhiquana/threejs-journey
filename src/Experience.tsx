fab10c4f254a3fba6432a7f179ac62abe635d763
wolizhiquana <wolizhiquana@gmail.com>
2023-07-03 05:58:51 +0800

    physics

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,67 +1,163 @@
+import { OrbitControls, useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import {
-  ContactShadows,
-  Environment,
-  Float,
-  Html,
-  PresentationControls,
-  Text,
-  useGLTF
-} from '@react-three/drei'
+  CuboidCollider,
+  CylinderCollider,
+  InstancedRigidBodies,
+  InstancedRigidBodyProps,
+  Physics,
+  RapierRigidBody,
+  RigidBody
+} from '@react-three/rapier'
+import { Perf } from 'r3f-perf'
+import { useMemo, useRef, useState } from 'react'
+import { Euler, Quaternion } from 'three'
 
 export default function Experience() {
-  const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
-  )
+  const [hitSound] = useState(() => new Audio('hit.mp3'))
+
+  const { scene: originium } = useGLTF('hamburger.glb')
+
+  const cube = useRef<RapierRigidBody>(null!)
+  const twister = useRef<RapierRigidBody>(null!)
+
+  const cubeCount = 1000
+  const cubes = useRef<RapierRigidBody[]>(null)
+  const instances = useMemo(() => {
+    const instances: InstancedRigidBodyProps[] = []
+
+    for (let i = 0; i < cubeCount; i++) {
+      const scaleRandom = Math.random()
+      instances.push({
+        key: 'instance_' + Math.random(),
+        position: [
+          (Math.random() - 0.5) * 8,
+          6 + i * 0.2,
+          (Math.random() - 0.5) * 8
+        ],
+        scale: [scaleRandom, scaleRandom, scaleRandom]
+      })
+    }
+
+    return instances
+  }, [])
+
+  const handleJump = () => {
+    const mass = cube.current.mass()
+    cube.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 }, true)
+    cube.current.applyTorqueImpulse(
+      {
+        x: Math.random() - 0.5,
+        y: Math.random() - 0.5,
+        z: Math.random() - 0.5
+      },
+      true
+    )
+  }
+
+  useFrame((state) => {
+    const time = state.clock.getElapsedTime()
+
+    const eulerRotation = new Euler(0, time * 3, 0)
+    const quaternionRotation = new Quaternion()
+    quaternionRotation.setFromEuler(eulerRotation)
+    twister.current.setNextKinematicRotation(quaternionRotation)
+
+    const angle = time * 0.5
+    const x = Math.cos(angle) * 2
+    const z = Math.sin(angle) * 2
+    twister.current.setNextKinematicTranslation({ x, y: -0.8, z })
+  })
+
+  const collisionEnter = () => {
+    hitSound.currentTime = 0
+    hitSound.volume = Math.random()
+    hitSound.play()
+  }
 
   return (
     <>
-      <Environment preset='city' />
+      <Perf position='top-left' />
 
-      <color args={['#241a1a']} attach='background' />
+      <OrbitControls makeDefault />
 
-      <PresentationControls
-        global
-        snap
-        rotation={[0.13, 0.1, 0]}
-        polar={[-0.4, 0.2]}
-        azimuth={[-1, 0.75]}
-        config={{ mass: 2, tension: 400 }}
-      >
-        <Float rotationIntensity={0.4}>
-          {/* Screen colr */}
-          <rectAreaLight
-            width={2.5}
-            height={1.65}
-            intensity={65}
-            color={'#867f7f'}
-            rotation={[0.1, Math.PI, 0]}
-            position={[0, 0.55, -1.55]}
+      <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <Physics gravity={[0, -9.81, 0]}>
+        <RigidBody colliders='ball'>
+          <mesh castShadow position={[-1.5, 2, 0]}>
+            <sphereGeometry />
+            <meshStandardMaterial color='orange' />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody
+          ref={cube}
+          gravityScale={1}
+          restitution={0}
+          friction={0.7}
+          colliders={false}
+          onCollisionEnter={collisionEnter}
+          onCollisionExit={() => console.log('exit')}
+          onSleep={() => console.log('sleep')}
+          onWake={() => console.log('wake')}
+        >
+          <CuboidCollider
+            args={[0.5, 0.5, 0.5]}
+            position={[1.5, 2, 0]}
+            mass={0.5}
           />
-          {/* Model and screen */}
-          <primitive object={scene} position-y={-1.2}>
-            <Html
-              transform
-              wrapperClass='htmlScreen'
-              distanceFactor={1.17}
-              position={[0, 1.56, -1.4]}
-              rotation-x={-0.256}
-            >
-              <iframe src='https://game-hub-two.vercel.app/' />
-            </Html>
-          </primitive>
-          <Text
-            font='bangers-v20-latin-regular.woff'
-            position={[2, 0.75, 0.75]}
-            rotation-y={-1.25}
-            maxWidth={2}
-            textAlign='center'
+          <mesh
+            // ref={cube}
+            castShadow
+            position={[1.5, 2, 0]}
+            rotation-x={Math.PI * 0.5}
+            onClick={handleJump}
           >
-            BRUNO SIMON
-          </Text>
-        </Float>
-      </PresentationControls>
+            <boxGeometry />
+            <meshStandardMaterial color='mediumpurple' />
+          </mesh>
+        </RigidBody>
 
-      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} />
+        <RigidBody type='fixed' friction={0.7}>
+          <mesh receiveShadow position-y={-1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
+            <meshStandardMaterial color='greenyellow' />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody
+          ref={twister}
+          position={[0, -0.8, 0]}
+          friction={0}
+          type='kinematicPosition'
+        >
+          <mesh castShadow scale={[0.4, 0.4, 4]}>
+            <boxGeometry />
+            <meshStandardMaterial color='red' />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody colliders={false} position={[0, 4, 0]}>
+          <CylinderCollider args={[0.5, 1.25]} />
+          <primitive object={originium} scale={0.25} />
+        </RigidBody>
+
+        <RigidBody type='fixed'>
+          <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, 5.5]} />
+          <CuboidCollider args={[5, 2, 0.5]} position={[0, 1, -5.5]} />
+          <CuboidCollider args={[0.5, 2, 5]} position={[5.5, 1, 0]} />
+          <CuboidCollider args={[0.5, 2, 5]} position={[-5.5, 1, 0]} />
+        </RigidBody>
+
+        <InstancedRigidBodies ref={cubes} instances={instances}>
+          <instancedMesh castShadow args={[undefined, undefined, cubeCount]}>
+            <boxGeometry />
+            <meshStandardMaterial color='red' />
+          </instancedMesh>
+        </InstancedRigidBodies>
+      </Physics>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,12 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Canvas
+      shadows
       camera={{
         fov: 45,
         near: 0.1,
         far: 200,
-        position: [-3, 1.5, 4]
+        position: [4, 2, 6]
       }}
     >
       <Experience />
